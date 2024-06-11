942fa47e0b50893e4e6d430e51a5e6a240441fdc
Dafna Nicole <111672427+thvx@users.noreply.github.com>
2024-06-10 22:54:12 -0500

    Agregando estilo dark

--- emprende-conecta-frontend/src/app/Test/testComponentes.tsx
+++ emprende-conecta-frontend/src/app/Test/testComponentes.tsx
@@ -1,13 +1,10 @@
-import {useState, useEffect} from 'react';
-
 function App() {
   
   return (
-    <div className="app-container min-h-screen bg-white">
-        <h1 className="text-3xl font-bold text-black underline text-center">Hello world!</h1>
+    <div className="app-container min-h-screen">
+        <h1 className="text-3xl font-bold text-white underline text-center">Hello world!</h1>
     </div>
   )
 }
-
 
 export default App;
