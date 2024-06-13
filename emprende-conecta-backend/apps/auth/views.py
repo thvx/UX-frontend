41ac25f5729429b93e58f52633b5d6d1ed6aa92d
Josue072003 <josue.espinoza2@unmsm.edu.pe>
2024-06-13 09:31:52 -0500

    backend avance

--- emprende-conecta-backend/apps/auth/views.py
+++ emprende-conecta-backend/apps/auth/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import PersonaSerializer
+from .models import Persona
 
 # Create your views here.
+class PersonaViews(viewsets.ModelViewSet):
+    serializer_class = PersonaSerializer
+    queryset = Persona.objects.all()
