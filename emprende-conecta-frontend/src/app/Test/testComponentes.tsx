97b7ca0a453d96cf0f387222c83ba2a72f06d750
Josue072003 <josue.espinoza2@unmsm.edu.pe>
2024-06-12 21:50:40 -0500

    avance breadcumbs

--- emprende-conecta-frontend/src/app/Test/testComponentes.tsx
+++ emprende-conecta-frontend/src/app/Test/testComponentes.tsx
@@ -1,11 +1,26 @@
-import {useState, useEffect} from 'react';
+import BreadCrumbs from '../../components/Atoms/Navigation/breadcumbs/breadcumbs';
+
+const breadcrumbData = [
+  {
+    title: 'Home',
+    iconLink: 'http://www.w3.org/2000/svg',
+  },
+  {
+    title: 'Documents',
+    iconLink: 'http://www.w3.org/2000/svg',
+  },
+  {
+    title: 'Add Document',
+    iconLink: 'http://www.w3.org/2000/svg',
+  },
+];
 
 function App() {
   
   return (
-    <div className="app-container min-h-screen bg-white">
-        <h1 className="text-3xl font-bold text-black underline text-center">Hello world!</h1>
-    </div>
+    <>
+  <BreadCrumbs items={breadcrumbData} />
+    </>
   )
 }
 
