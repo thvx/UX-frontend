b0b6808cde99fb6664fc54b0fca850b52fb608b0
JorgeIP <jorgep.ipanaquep@gmail.com>
2024-06-12 20:07:04 -0500

    chore: probar componentes

--- emprende-conecta-frontend/src/app/Test/testComponentes.tsx
+++ emprende-conecta-frontend/src/app/Test/testComponentes.tsx
@@ -1,8 +1,12 @@
-function App() {
+import Checkbox from "../../components/Atoms/Input/checkbox/Checkbox.tsx";
+
+const App: React.FC = () => {
   
   return (
     <div className="app-container min-h-screen">
-        <h1 className="text-3xl font-bold text-white underline text-center">Hello world!</h1>
+        <div className="flex flex-col items-center gap-6 p-6">
+          <Checkbox size="sm" />
+        </div>
     </div>
   )
 }
