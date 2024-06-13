85b0f72a8c761c9edc55b928e7d32968b01f1ea1
JorgeIP <jorgep.ipanaquep@gmail.com>
2024-06-13 03:50:08 -0500

    chore: probar componentes

--- emprende-conecta-frontend/src/app/Test/testComponentes.tsx
+++ emprende-conecta-frontend/src/app/Test/testComponentes.tsx
@@ -4,6 +4,13 @@ import FileInput from "../../components/Atoms/Input/file/FileInput.tsx";
 import { RadioItem } from "../../components/Atoms/Input/radio/radioType.ts";
 import RadioList from "../../components/Organism/RadioList/RadioList.tsx";
 import Select from "../../components/Atoms/Input/select/Select.tsx";
+import TextInput from "../../components/Atoms/Input/text/TextInput.tsx";
+import TextInputMail from "../../components/Atoms/Input/text/TextInputMail.tsx";
+import TextInputSearch from "../../components/Atoms/Input/text/TextInputSearch.tsx";
+import TextInputUsername from "../../components/Atoms/Input/text/TextInputUsername.tsx";
+import TextInputPassword from "../../components/Atoms/Input/text/TextInputPassword.tsx";
+import TextArea from "../../components/Atoms/Input/textarea/Textarea.tsx";
+import Toggle from "../../components/Atoms/Input/toggle/Toggle.tsx";
 
 const App: React.FC = () => {
   const radioItems: RadioItem[] = [
@@ -15,10 +22,17 @@ const App: React.FC = () => {
   return (
     <div className="app-container min-h-screen">
         <div className="flex flex-col items-center gap-6 p-6">
-          <Checkbox size="checkbox-sm" checkboxType="checkbox-error" />
+          <Checkbox size="checkbox-sm" />
           <FileInput />
           <RadioList items={radioItems} />
           <Select size="select-xs" items={['Seleccionar elemento', 'zzzz', 'dsdsad']} />
+          <TextInput size="input-md" placeholder="Escribe..." />
+          <TextInputMail placeholder="Correo" size="input-md" />
+          <TextInputSearch placeholder="Buscar" size="input-md" />
+          <TextInputUsername placeholder="Usuario" size="input-md" />
+          <TextInputPassword placeholder="Contraseña" size="input-md" />
+          <TextArea placeholder="Ingresa texto" size="textarea-md" />
+          <Toggle size="toggle-xs" />
         </div>
     </div>
   )
