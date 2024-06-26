f24594b48e2b0d50e73e07598f0d9328d0565e73
Dafna Nicole <111672427+thvx@users.noreply.github.com>
2024-06-25 22:42:16 -0500

    Add homepage al routing

--- emprende-conecta-frontend/src/App.tsx
+++ emprende-conecta-frontend/src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css'
 import Preregistro from './app/Registro/preregistro'
-import TestComponentes from './app/Test/testComponentes'
 import Dashboard from './app/Dashboard/dashboard'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import RegistroComprador from '../src/app/Registro/registroComprador'
 import RegistroVendedor from '../src/app/Registro/registroVendedor'
 import Login from './app/Login/login'
+import Homepage from './app/Homepage/homepage'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TestComponentes />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/preregistro" element={<Preregistro />} />
         <Route path="/registro/comprador" element={<RegistroComprador/>} />
