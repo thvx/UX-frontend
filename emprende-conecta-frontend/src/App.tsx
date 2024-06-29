371be6470736ad17948b1723aa8bbde4f453221c
Rosmeri Ccanto Flores <rosmeri.ccanto@unmsm.edu.pe>
2024-06-29 13:56:11 -0500

    Agregando #28-MisFavoritosproductostienda

--- emprende-conecta-frontend/src/App.tsx
+++ emprende-conecta-frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Preregistro from './app/Registro/preregistro'
 import Dashboard from './app/Dashboard/dashboard'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RegistroComprador from '../src/app/Registro/registroComprador'
 import RegistroVendedor from '../src/app/Registro/registroVendedor'
 import Login from './app/Login/login'
 import Homepage from './app/Homepage/homepage'
+import ProductosFav from './app/Productosfav/Productos'
 
 function App() {
 
@@ -15,9 +16,10 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/preregistro" element={<Preregistro />} />
-        <Route path="/registro/comprador" element={<RegistroComprador/>} />
-        <Route path="/registro/vendedor" element={<RegistroVendedor/>} />
-        <Route path="/login" element = {<Login/>} />
+        <Route path="/registro/comprador" element={<RegistroComprador />} />
+        <Route path="/registro/vendedor" element={<RegistroVendedor />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/productosfav' element={<ProductosFav />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ emprende-conecta-frontend/src/components/Templates/ProductosFav/Productos.tsx
@@ -0,0 +1,158 @@
+import { useState } from "react";
+
+import SearchInput from "../../Atoms/Input/search/search";
+
+function Producto() {
+    const productos = [
+        {
+            id: 1,
+            imagen:
+                "https://oechsle.vteximg.com.br/arquivos/ids/17298402-1000-1000/imageUrl_1.jpg?v=638418759863570000",
+            nombre: "Camisa estampada",
+            precio: 25.99,
+        },
+        {
+            id: 2,
+            imagen: "https://i.ebayimg.com/images/g/7AEAAOSwhm9iNzkR/s-l500.jpg",
+            nombre: "Zapatos deportivos",
+            precio: 59.99,
+        },
+        {
+            id: 3,
+            imagen:
+                "https://portape.vtexassets.com/arquivos/ids/162317/0000010496--1-.jpg?v=638089668629600000",
+            nombre: "Mochila azul",
+            precio: 35.99,
+        },
+        {
+            id: 4,
+            imagen:
+                "https://promart.vteximg.com.br/arquivos/ids/7544937-1000-1000/image-caf1e09de32d46c292d97edd29f56bd7.jpg?v=638335007876000000",
+            nombre: "Auriculares inalámbricos",
+            precio: 89.99,
+        },
+        {
+            id: 5,
+            imagen:
+                "https://promart.vteximg.com.br/arquivos/ids/7118854-1000-1000/image-526c21fcc2fc40c49ca858cbfb26954d.jpg?v=638223677021000000",
+            nombre: "Reloj inteligente",
+            precio: 199.99,
+        },
+    ];
+    return (
+        <div className='flex gap-3 flex-wrap '>
+            {productos.map((producto) => (
+                <div key={producto.id} className=' max-w-52 bg-[#0B3954]'>
+                    <div className='w-52 h-52 rounded-2xl overflow-hidden'>
+                        <img
+                            src={producto.imagen}
+                            alt={producto.nombre}
+                            className='h-full w-full object-cover'
+                        />
+                    </div>
+                    <div className='mt-2 '>
+                        <h2 className='font-bold text-lg text-white'>{producto.nombre}</h2>
+                        <p className='font-bold text-base text-white'>
+                            S/.{producto.precio}
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+function VistaTienda() {
+    const tiendas = [
+        {
+            id: 1,
+            image:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_ctjl3Hs1EdLvK_eyMDXUpsncTjOEIcZfsA&s",
+            name: "La Casa del Café",
+        },
+        {
+            id: 2,
+            image:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSezhV9H1-AeiD_bLyGJKVSlF0gncY14c_x_w&s",
+            name: "Tienda Verde",
+        },
+        {
+            id: 3,
+            image:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzIT1E7OI2m01P-PCmGzOgSCgW_ohV0nabNQ&s",
+            name: "Moda Urbana",
+        },
+        {
+            id: 4,
+            image:
+                "https://gestion.pe/resizer/rWPejMxYEMOz3qtTfkiIan8UEwQ=/1200x1200/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/XCPGF6QI6VDDNEHDSF7AE3YOWQ.JPG",
+            name: "ElectroHogar",
+        },
+        {
+            id: 5,
+            image: "https://forbes.es/wp-content/uploads/2020/07/IMG_4001.jpg",
+            name: "Libros & Más",
+        },
+    ];
+    return (
+        <div className='flex gap-3 flex-wrap bg-[#0B3954]'>
+            {tiendas.map((tienda) => (
+                <div key={tienda.id} className='max-w-52 bg-[#0B3954] '>
+                    <div className='w-52 h-52 rounded-2xl overflow-hidden'>
+                        <img
+                            src={tienda.image}
+                            alt={tienda.name}
+                            className='h-full w-full object-cover'
+                        />
+                    </div>
+                    <div className='mt-2'>
+                        <h2 className='font-bold text-lg text-white'>{tienda.name}</h2>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+function FavoritesTemplate() {
+    const [activeTab, setActiveTab] = useState("producto");
+    const [word, setWord] = useState("");
+    const handleText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setWord(e.target.value);
+    };
+    return (
+        <div className='bg-[#0B3954] flex-1'>
+            <div className='container mx-auto max-w-2xl pt-10'>
+                <h2 className=' text-center text-3xl mb-2 text-white font-semibold '>
+                    Mis Favoritos
+                </h2>
+
+                <SearchInput placeholder='Buscar' value={word} onChange={handleText} />
+
+                <div className='flex justify-center gap-4 my-4'>
+                    <button
+                        className={`px-4 py-2 rounded-md text-[#0B3954] font-semibold ${activeTab === "producto" ? "bg-[#E0FF4F] " : "bg-[#BDD6E8]"
+                            }`}
+                        onClick={() => setActiveTab("producto")}
+                    >
+                        Productos
+                    </button>
+                    <button
+                        className={`px-4 py-2 rounded-md text-[#0B3954] font-semibold ${activeTab === "vistaTienda" ? "bg-[#E0FF4F] " : "bg-[#BDD6E8]"
+                            }`}
+                        onClick={() => setActiveTab("vistaTienda")}
+                    >
+                        Tiendas
+                    </button>
+                </div>
+                <div>
+                    {activeTab === "producto" && <Producto />}
+                    {activeTab === "vistaTienda" && <VistaTienda />}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FavoritesTemplate;
