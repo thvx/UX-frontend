b04518ef2f43f6bb7851710c2732d22b3af763e4
Dafna Nicole <111672427+thvx@users.noreply.github.com>
2024-06-29 21:09:43 -0500

    Update: card AddProduct - actualizar cantidad de productos

--- emprende-conecta-frontend/src/components/Atoms/DataDisplay/Card/CardAddProduct.tsx
+++ emprende-conecta-frontend/src/components/Atoms/DataDisplay/Card/CardAddProduct.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 type CardProductosProps = { 
     image: string;
@@ -7,34 +7,43 @@ type CardProductosProps = {
     price: string;
     initialQuantity: number;
     extraClasses?: string;
+    onQuantityChange: (newQuantity: number) => void;
 };
 
-
-const CardProductos: React.FC<CardProductosProps> = ({ image, title, store, price, initialQuantity, extraClasses }) => {
+const CardProductos: React.FC<CardProductosProps> = ({ image, title, store, price, initialQuantity, extraClasses, onQuantityChange }) => {
     const [quantity, setQuantity] = useState(initialQuantity);
+
+    const parsePrice = (price: string) => {
+        const parsed = parseFloat(price.replace('S/', ''));
+        return isNaN(parsed) ? 0 : parsed;
+    };
+
+    useEffect(() => {
+        onQuantityChange(quantity);
+    }, [quantity, onQuantityChange]);
 
     const increaseQuantity = () => setQuantity(prevQuantity => prevQuantity + 1);
     const decreaseQuantity = () => setQuantity(prevQuantity => (prevQuantity > 0 ? prevQuantity - 1 : 0));
-    
+
+    const totalPrice = (parsePrice(price) * quantity).toFixed(2);
+
     return (
         <div className={`card max-w-72 bg-custom-lightgrey shadow-xl flex flex-row ${extraClasses}`}>
-        <div className="w-1/3 content-center ml-4">
-          <figure><img src={image} alt={title} className="w-full h-full object-cover" /></figure>
+            <div className="w-1/3 content-center ml-4">
+                <figure><img src={image} alt={title} className="w-full h-full object-cover" /></figure>
+            </div>
+            <div className="text-custom-blue p-4">
+                <h2 className="text-custom-blue font-bold">{title}</h2>
+                <p>{store}</p>
+                <p>S/ {totalPrice}</p>
+                <div className="card-actions flex justify-end items-center mt-2">
+                    <button onClick={decreaseQuantity} className="btn bg-custom-yellow border-none text-custom-blue">-</button>
+                    <span className="mx-2">{quantity}</span>
+                    <button onClick={increaseQuantity} className="btn bg-custom-yellow border-none text-custom-blue">+</button>
+                </div>
+            </div>
         </div>
-        <div className="card-body text-custom-blue p-4">
-          <h2 className="card-title text-custom-blue font-bold text-xl">
-            {title}
-          </h2>
-          <p>{store}</p>
-          <p>{price}</p>
-          <div className="card-actions flex justify-end items-center">
-            <button onClick={decreaseQuantity} className="btn bg-custom-yellow border-none text-custom-blue">-</button>
-            <span className="mx-2">{quantity}</span>
-            <button onClick={increaseQuantity} className="btn bg-custom-yellow border-none text-custom-blue">+</button>
-          </div>
-        </div>
-      </div>
     );
-  };
+};
 
 export default CardProductos;
