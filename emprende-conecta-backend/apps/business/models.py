b03da5c5b38b06661fd5891d81e92d8e269aa5ac
Lokitas01 <lukasameed12@gmail.com>
2024-07-11 00:24:39 -0500

    login vendedor y comprador

--- emprende-conecta-backend/apps/business/models.py
+++ emprende-conecta-backend/apps/business/models.py
@@ -67,3 +67,8 @@ class Emprendimiento(models.Model):
 
     class Meta:
         db_table = 'emprendimiento'
+
+    def save(self, *args, **kwargs):
+        if self.password and not self.password.startswith('pbkdf2_sha256'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
